b084aed6c7b159398857faba4f5760bf3f891fc7
TarieliCrcv <tsertsvadze2005@gmail.com>
2026-08-25 17:57:46 +0400

    first API version finished check README.md for instructionss to start and functions

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ const app: Express = express();
 app.use(express.json());
 app.use(logger);
 
+app.get("/", (req,res)=>{res.json("hello this is books api for testing")})
 app.use("/books", booksRouter);
 
 export default app;
--- src/routers/book.routes.ts
+++ src/routers/book.routes.ts
@@ -5,5 +5,8 @@ const router = Router();
 
 router.get("/", books.getBooks);
 router.get("/:id", books.getBook);
+router.post("/", books.uploadBook);
+router.patch("/", books.changeBook);
+router.delete("/:id", books.deleteBook);
 
 export default router;
